be9e1721bd65a47e2147839e1ac177a1660ebbf3
James Annesley <james.annesley@regnosys.com>
2020-08-27 11:20:18 +0100

    Add script

--- api-demo/demo.sh
+++ api-demo/demo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+go build
 ./apidemo -auth 6d+rsIzDeDVO/2U2+MhFpZTWnF4VehA9Mtf7FqhnW+i2f1ua6xBtpy7A5bXc7EpoYT/BxHxoSc5GxJHicdJXPA== -cookie ROSETTA_SESSION=1598372811.811.1666.342129 -project james.annesley_auth0_5ed7c36eeb3f4a001316dfbe/g/latest/MAS/SFA/MAS_2013
 
